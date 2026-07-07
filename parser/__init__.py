b1b651f3f90034a4463c96057322cfae92c5b144
LexterS999 <kaliostro18@gmail.com>
2026-07-07 20:17:20 +0500

    Create __init__.py

---
+++ parser/__init__.py
@@ -0,0 +1,14 @@
+"""
+Telegram ID Parser package
+"""
+
+from .core import Parser
+from .extractors import extract_telegram_ids
+from .utils import load_links_from_file, load_links_from_url
+
+__all__ = [
+    'Parser',
+    'extract_telegram_ids',
+    'load_links_from_file',
+    'load_links_from_url',
+]
