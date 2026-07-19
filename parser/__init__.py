a8999c8f17fdfcbd55022bae11c05b2630d3f461
LexterS999 <kaliostro18@gmail.com>
2026-07-19 19:03:56 +0500

    Update __init__.py

--- parser/__init__.py
+++ parser/__init__.py
@@ -4,11 +4,15 @@ Telegram ID Parser package
 
 from .core import Parser
 from .extractors import extract_telegram_ids
-from .utils import load_links_from_file, load_links_from_url
+from .utils import load_links_from_file, load_links_from_url, load_links_from_url_stream
+from .async_utils import fetch_all_links, fetch_links_from_url
 
 __all__ = [
     'Parser',
     'extract_telegram_ids',
     'load_links_from_file',
     'load_links_from_url',
+    'load_links_from_url_stream',
+    'fetch_all_links',
+    'fetch_links_from_url',
 ]
